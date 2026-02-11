ea7c451fcee24fc5d683e99cb855428c220e73f1
isaaczx2 <isaac14230709@gmail.com>
2026-02-11 15:05:04 -0300

    11/2

---
+++ aula01/script.js
@@ -0,0 +1,37 @@
+var nome="isaac"
+
+var idade=15;
+
+var altura=1.87;
+
+var dias= 16 * 367
+console.log( nome );
+console.log(  idade );
+console.log( altura );
+console.log(dias );
+
+document.write("<h1>ola,seja bem vindo ao javascript</h1>")
+var dias=idade*365
+
+document.write( nome+"<br>");
+document.write( idade+"<br>");
+document.write( altura+"<br>");
+document.write("<p style='color:red>voce ja viveu"+dias+ "dias</p>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
